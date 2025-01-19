c6aef0158d8c2eaeb84640ff2d68e0c4bc230296
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-01-19 23:07:25 +0200

    Solve 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+  let newWord = "";
+  for(let i = 0; i < times; i++){
+    newWord += word;
+  };
+  // if number of times to repeat is negative return error, else return new word
+  return times < 0 ? "ERROR": newWord;
 };
 
 // Do not edit below this line
