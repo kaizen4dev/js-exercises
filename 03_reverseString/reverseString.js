930fcb6872eb4b0ae04ff1811e5045581f5dc15b
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-01-19 23:16:24 +0200

    Solve 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  newStr = "";
+  for(let i = str.length; i > 0; i--){
+    newStr += str.at(i-1);
+  }
+  return newStr;
 };
 
 // Do not edit below this line
