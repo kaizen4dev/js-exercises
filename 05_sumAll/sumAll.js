c28212ef07d2219d6cafb9f138b719f80c04805b
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-01-20 14:24:06 +0200

    Solve 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+  // Error checking
+
+  // values are numbers
+  if(typeof(start && end) != "number"){
+    return "ERROR";
+  }
+
+  // numbers are integers
+  if(!Number.isInteger(start) || !Number.isInteger(end)){
+    return "ERROR";
+  }
+
+  // numbers are in correct order
+   if(start > end){ 
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+
+  // numbers are positive
+  if(start < 0 || end < 0){ 
+    return "ERROR";
+  } 
+
+
+  // calculate result
+  let result = 0; 
+  for(let i = start; i <= end; i++){
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
