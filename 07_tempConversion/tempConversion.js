34ac49234c7866b81c86350143c60cd76782706d
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-01-20 15:04:52 +0200

    Solve 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  const C = (F - 32) * (5/9);
+
+  if(Number.isInteger(C)){
+    return C;
+  }
+
+  return Number(C.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  const F = C * (9/5) + 32;
+
+  if(Number.isInteger(F)){
+    return F;
+  }
+
+  return Number(F.toFixed(1));
 };
 
 // Do not edit below this line
