a96baf635586fd3f80fe1f75e756ad8f969abe0d
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-01-20 14:39:42 +0200

    Solve 6th exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  // century years
+  if(year % 100 == 0){
+    return year % 400 == 0 ? true : false;
+  }
 
+  // normal years
+  return year % 4 == 0 ? true : false;
 };
 
 // Do not edit below this line
