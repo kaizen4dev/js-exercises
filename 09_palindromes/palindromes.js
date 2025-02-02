368b7b9a33c77eac7e4903ec802fce5b59cce034
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-02-02 14:52:30 +0200

    Solve 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // using strings //
 
+  // let formated = string.toLowerCase().replace(/\W/g, "");
+
+  // let newString = "";
+  // for(let i = formated.length - 1; i >= 0; i--){
+  //   newString += formated.at(i)
+  // }
+
+  // return formated == newString;
+
+
+  // using arrays //
+  
+  let array = string.toLowerCase().replace(/\W/g, "").split("");
+
+  let newArray = [];
+  array.map((item) => newArray.unshift(item));
+
+  for(key in array){
+    if(array[key] != newArray[key]) return false;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
