e028840791db651e5f555bc993ef9153e49012d2
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-02-02 16:32:34 +0200

    Solve 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  // handle negative numbers
+  if(num < 0) return "OOPS";
 
+  // create array representation of fibonacci sequence
+  let seq = [0, 1];
+  // if needed(i.e. num is 2 or more), calculate sequence up to targeted number
+  while(seq.length <= num){
+    seq.push(seq[seq.length - 1] + seq[seq.length - 2])
+  }
+  // return result
+  return seq[num];
 };
 
 // Do not edit below this line
