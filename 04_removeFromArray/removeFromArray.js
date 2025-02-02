4011366b3614a7fb50fd7529b9dd55363edfb50c
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-02-02 12:54:19 +0200

    Solve 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+  // delete values from args in array
+  for(value of args){
+    for(let key = 0; key < array.length; key++){
+      if(array[key] === value) delete array[key];
+    }
+  }
+
+  // crate new array without "undefined" holes
+  let newArray = [];
+  for(let key = 0; key < array.length; key++){
+    if(typeof(array[key]) != 'undefined') newArray.push(array[key]);
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
