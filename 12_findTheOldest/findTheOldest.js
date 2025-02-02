ca5ae6e5fcadce708ddd86cad5f8e6c31deb4669
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-02-02 17:10:52 +0200

    Solve 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  function calcAge(person){
+    let currentYear = 2025;
 
+    if(!person.yearOfDeath){
+      return currentYear - person.yearOfBirth;
+    }
+
+    return person.yearOfDeath - person.yearOfBirth;
+  }
+
+  return people.reduce((oldest, current) => {
+    return calcAge(current) > calcAge(oldest) ? current : oldest;
+  });
 };
 
 // Do not edit below this line
